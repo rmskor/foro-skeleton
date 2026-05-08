0ff2da77d037180255ac5780d72d4682c784b5e2
woof <126932561+wooffff@users.noreply.github.com>
2026-05-08 21:53:16 +0800

    removed star temporarily (#52)

--- src/components/dashboard/CompetitionCard.tsx
+++ src/components/dashboard/CompetitionCard.tsx
@@ -89,7 +89,7 @@ export default function CompetitionCard({ data, rotation = 0 }: CompetitionCardP
 
           {/* TEMP we need these to actually do something as buttons. */}
           <div className="mt-1 flex shrink-0 gap-2.5 text-gray-400">
-            <button aria-label="Add to wishlist" className="transition-colors hover:text-amber-400">
+            {/* <button aria-label="Add to wishlist" className="transition-colors hover:text-amber-400">
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g>
                   <path 
@@ -101,7 +101,7 @@ export default function CompetitionCard({ data, rotation = 0 }: CompetitionCardP
                   />
                 </g>
               </svg>
-            </button>
+            </button> */}
             <button aria-label="Save competition" className="transition-colors hover:text-blue-500">
               <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M19 21L12 16L5 21V5C5 4.46957 5.21071 3.96086 5.58579 3.58579C5.96086 3.21071 6.46957 3 7 3H17C17.5304 3 18.0391 3.21071 18.4142 3.58579C18.7893 3.96086 19 4.46957 19 5V21Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
